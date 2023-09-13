3d82fffa9b46d3ca179c6e8f3d1de30c94259acf
GreasyCat <131315c@gmail.com>
2023-09-13 14:00:10 -0700

    Add region detection

---
+++ Assets/Scripts/RegionTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string _regionName;
+    private void Start()
+    {
+        _regionName = transform.parent.name;
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MazeController.instance.EnterRegion(_regionName);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        MazeController.instance.ExitRegion();
+    }
+}
