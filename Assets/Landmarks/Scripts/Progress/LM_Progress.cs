bc81fba870a1598de915406d7e7c3957ea4d1237
GreasyCat <131315c@gmail.com>
2023-11-27 11:51:36 -0800

    now resume position and rotation

--- Assets/Landmarks/Scripts/Progress/LM_Progress.cs
+++ Assets/Landmarks/Scripts/Progress/LM_Progress.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using Landmarks.Scripts.Debugging;
@@ -16,6 +18,9 @@ namespace Landmarks.Scripts.Progress
         public static readonly string ApplicationName = "Landmarks";
         public static readonly string SaveFolderName = "saves";
 
+        // Singleton variables
+        private Experiment _experiment;
+
         [SerializeField] public bool resumeLastSave = true;
 
         // Saves-related variables
@@ -26,6 +31,7 @@ namespace Landmarks.Scripts.Progress
 
         // Task-related variables
         private XmlNode _currentSaveNode;
+        private Dictionary<string, XmlNode> _taskNodeLookup;
         private Queue<KeyValuePair<string, string>> _attributeQueue;
 
 
@@ -33,6 +39,8 @@ namespace Landmarks.Scripts.Progress
         [SerializeField] private bool deleteCurrentSaveFileOnEditorQuit = false;
         private int _depth = 0;
         private int _line = 1;
+
+        private Coroutine ReportingCoroutine { get; set; }
 
 
         //**************************************************************
@@ -55,6 +63,8 @@ namespace Landmarks.Scripts.Progress
         private void Start()
         {
             _attributeQueue = new Queue<KeyValuePair<string, string>>();
+            _experiment = FindObjectOfType<Experiment>();
+            _taskNodeLookup = new Dictionary<string, XmlNode>();
         }
 
         public void InitializeSave(string savePath = "")
@@ -95,10 +105,194 @@ namespace Landmarks.Scripts.Progress
             return null;
         }
 
+        public void UpdateAttributeAfterWritten(XmlNode node, string key, string value)
+        {
+            XmlNode.UpdateAttribute(currentSaveFile, node, key, value);
+        }
+
+        public void UpdateAttributesAfterWritten(XmlNode node, Dictionary<string, string> dictionary)
+        {
+            XmlNode.UpdateAttributes(currentSaveFile, node, dictionary);
+        }
+
+        public void ResumeLastNavigationParameters(NavigationTask task)
+        {
+            var parentNode = _currentSaveNode;
+            if (parentNode == null)
+            {
+                LM_Debug.Instance.Log("No parent node found", 3);
+                return;
+            }
+
+            if (!parentNode.HasAttribute("timeRemaining"))
+            {
+                LM_Debug.Instance.Log("No time remaining attribute found", 3);
+                LM_Debug.Instance.Log("Current Node: " + parentNode.Name, 3);
+                LM_Debug.Instance.Log(parentNode.GetSomeAttributesToString(3), 3);
+                return;
+            }
+
+            var timeString = parentNode.GetAttribute("timeRemaining");
+            if (float.TryParse(timeString, out var time))
+            {
+                task.timeAllotted = time;
+                LM_Debug.Instance.Log("Set time remaining to " + time, 3);
+            }
+
+            parentNode.RemoveAttribute("timeRemaining");
+        }
+
+        public void ResumeLastExplorationTimer(TL_ExplorationTask_Select task)
+        {
+            var parentNode = _currentSaveNode;
+            if (parentNode == null)
+            {
+                LM_Debug.Instance.Log("No parent node found", 3);
+                return;
+            }
+
+            if (!parentNode.HasAttribute("timeRemaining"))
+            {
+                LM_Debug.Instance.Log("No time remaining attribute found", 3);
+                LM_Debug.Instance.Log("Current Node: " + parentNode.Name, 3);
+                LM_Debug.Instance.Log(parentNode.GetSomeAttributesToString(3), 3);
+                return;
+            }
+
+            var timeString = parentNode.GetAttribute("timeRemaining");
+            if (float.TryParse(timeString, out var time))
+            {
+                task.timeAllotted = time;
+                LM_Debug.Instance.Log("Set time remaining to " + time, 3);
+            }
+
+            parentNode.RemoveAttribute("timeRemaining");
+        }
+
+        public void ResumeLastExplorationPlayerPosition(Transform startTransform)
+        {
+            var nodeSearch = _taskNodeLookup.Where(p => p.Value.HasAttribute("x")).ToList();
+            if (nodeSearch.Count == 1)
+            {
+                var node = nodeSearch[0].Value;
+                if (float.TryParse(node.GetAttribute("x"), out var x) &&
+                    float.TryParse(node.GetAttribute("z"), out var z))
+                {
+                    var y = startTransform.position.y; // do not change the height
+                    startTransform.position = new Vector3(x, y, z);
+                }
+
+                if (float.TryParse(node.GetAttribute("rx"), out var rx) &&
+                    float.TryParse(node.GetAttribute("ry"), out var ry) &&
+                    float.TryParse(node.GetAttribute("rz"), out var rz) &&
+                    float.TryParse(node.GetAttribute("rw"), out var rw))
+                {
+                    var rotation = new Quaternion(rx, ry, rz, rw);
+                    // rotate on y axis by -90 degrees
+                    rotation *= Quaternion.Euler(0, -90, 0);
+                    startTransform.rotation = rotation;
+                }
+                
+                // remove attributes
+                node.RemoveAttribute("x");
+            }
+            else
+            {
+                LM_Debug.Instance.Log("Found " + nodeSearch.Count + " nodes with position attribute", 3);
+            }
+        }
+
+        public void StartExplorationCoroutine(TL_ExplorationTask_Select task)
+        {
+            ReportingCoroutine = StartCoroutine(ExplorationUpdatingCoroutine(task));
+        }
+
+        public void StartNavigationCoroutine(NavigationTask task)
+        {
+            ReportingCoroutine = StartCoroutine(NavigationUpdatingCoroutine(task));
+        }
+
+        public void StopReportingCoroutine(ExperimentTask task)
+        {
+            if (ReportingCoroutine == null) return;
+
+            StopCoroutine(ReportingCoroutine);
+            UpdateAttributeAfterWritten(task.parentTask.taskNodeToBeWritten, "timeRemaining", "");
+        }
+
+        private IEnumerator NavigationUpdatingCoroutine(NavigationTask task)
+        {
+            var parentNode = task.parentTask.taskNodeToBeWritten;
+            while (true)
+            {
+                UpdateAttributeAfterWritten(parentNode, "timeRemaining",
+                    task.timeRemaining.ToString(CultureInfo.InvariantCulture));
+                yield return new WaitForSecondsRealtime(1f);
+            }
+        }
+
+        private IEnumerator ExplorationUpdatingCoroutine(TL_ExplorationTask_Select task)
+        {
+            var parentNode = task.parentTask.taskNodeToBeWritten;
+            while (true)
+            {
+                var avatarPosition = task.GetPlayerPosition();
+                var avatarRotation = task.GetPlayerRotation();
+
+                var attributes = new Dictionary<string, string>
+                {
+                    {"timeRemaining", task.timeRemaining.ToString(CultureInfo.InvariantCulture)},
+                    {"x", avatarPosition.x.ToString(CultureInfo.InvariantCulture)},
+                    {"y", avatarPosition.y.ToString(CultureInfo.InvariantCulture)},
+                    {"z", avatarPosition.z.ToString(CultureInfo.InvariantCulture)},
+                    {"rx", avatarRotation.x.ToString(CultureInfo.InvariantCulture)},
+                    {"ry", avatarRotation.y.ToString(CultureInfo.InvariantCulture)},
+                    {"rz", avatarRotation.z.ToString(CultureInfo.InvariantCulture)},
+                    {"rw", avatarRotation.w.ToString(CultureInfo.InvariantCulture)},
+                };
+
+                UpdateAttributesAfterWritten(parentNode, attributes);
+                yield return new WaitForSecondsRealtime(1f);
+            }
+        }
+
+        // public Vector3? GetLastPositionFromCurrentNode()
+        // {
+        //     var node = _currentSaveNode;
+        //     if (float.TryParse(node.GetAttribute("x"), out var x) &&
+        //         float.TryParse(node.GetAttribute("y"), out var y) && 
+        //         float.TryParse(node.GetAttribute("z"), out var z))
+        //     {
+        //         return new Vector3(x, y, z);
+        //     }
+        //
+        //     return null;
+        // }
+        //
+        // public Quaternion? GetLastRotationFromCurrentNode()
+        // {
+        //     var node = _currentSaveNode;
+        //     if (float.TryParse(node.GetAttribute("rx"), out var x) &&
+        //         float.TryParse(node.GetAttribute("ry"), out var y) &&
+        //         float.TryParse(node.GetAttribute("rz"), out var z) &&
+        //         float.TryParse(node.GetAttribute("rw"), out var w))
+        //     {
+        //         return new Quaternion(x, y, z, w);
+        //     }
+        //
+        //     return null;
+        // }
+
 
         //**************************************************************
         // Task-related methods
         //**************************************************************
+
+        private static uint GetUid(Component task)
+        {
+            // Get UID component from transform
+            return task.TryGetComponent(out Uid uid) ? uid.ID : 0;
+        }
 
         /// <summary>
         /// Record the start of a task
@@ -116,23 +310,30 @@ namespace Landmarks.Scripts.Progress
             {
                 {"name", task.name},
                 {"line", _line.ToString()},
-                {"depth", _depth.ToString()}
+                {"depth", _depth.ToString()},
+                {"uid", GetUid(task).ToString()}
             };
 
             var attributePairs = attributes.Select(pair => $"{pair.Key}=\"{pair.Value}\"").ToList();
             LM_Debug.Instance.Log($"Attributes: {string.Join(", ", attributePairs)}", 2);
+
+            if (_currentSaveNode != null && _currentSaveNode.HasAttributeEqualTo("uid", attributes["uid"]))
+            {
+                task.taskNodeLoaded = _currentSaveNode;
+                LM_Debug.Instance.Log($"Found matching uid for {attributes["name"]}: {attributes["uid"]}", 2);
+            }
 
             TrySkip(task); // This may add some attributes to the attributes dictionary
 
             MoveAllAttributesFromQueue(attributes); // Push all the attributes from the queue to the dictionary
 
             var newNode = new XmlNode("Task", attributes);
-            task.taskNode = newNode;
+            task.taskNodeToBeWritten = newNode;
 
             var text = XmlNode.BuildOpeningString(newNode);
             WriteToCurrentSaveFileSync(text);
-            _line++;
 
+            _line++;
             _depth++;
 
 
@@ -144,7 +345,7 @@ namespace Landmarks.Scripts.Progress
         }
 
         public bool CheckIfResumeCurrentNode(ExperimentTask task) =>
-            resumeLastSave && _currentSaveNode?.Name != null && _currentSaveNode.Name == task.name;
+            resumeLastSave && _currentSaveNode.HasAttributeEqualTo("uid", GetUid(task).ToString());
 
 
         /// <summary>
@@ -186,12 +387,12 @@ namespace Landmarks.Scripts.Progress
                 return;
             }
 
-            if (task.name != _currentSaveNode.Name)
+            if (!_currentSaveNode.HasAttributeEqualTo("uid", GetUid(task).ToString()))
             {
-                LM_Debug.Instance.Log($"Task name not match: {task.name} {_currentSaveNode.Name}", 1);
+                LM_Debug.Instance.Log($"UID not match: {task.name} {_currentSaveNode.Name}", 1);
+                LM_Debug.Instance.Log($"UID: {GetUid(task)} {_currentSaveNode.GetAttribute("uid")}", 1);
                 return;
             }
-
 
             if (!_currentSaveNode.HasAttributeEqualTo("completed", "true"))
             {
@@ -249,7 +450,8 @@ namespace Landmarks.Scripts.Progress
                         }
                         else
                         {
-                            LM_Debug.Instance.Log($"Cannot find LM_IncrementLists component in {childTransform.name}", 1);
+                            LM_Debug.Instance.Log($"Cannot find LM_IncrementLists component in {childTransform.name}",
+                                1);
                         }
                     }
 
@@ -292,7 +494,7 @@ namespace Landmarks.Scripts.Progress
             lastSaveStack = File.ReadAllText(lastSaveFile).Split('\n').ToList();
             RemoveEmptyLines(lastSaveStack);
 
-            _currentSaveNode = XmlNode.ParseFromLines(lastSaveStack);
+            _currentSaveNode = XmlNode.ParseFromLines(lastSaveStack, _taskNodeLookup, "uid");
             XmlNode.MoveToNextNode(ref _currentSaveNode);
 
             LM_Debug.Instance.Log(_currentSaveNode.HierarchyToString(0), 2);
